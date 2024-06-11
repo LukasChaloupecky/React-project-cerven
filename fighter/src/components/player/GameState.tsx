fc61806eb1a90317b395e2b829d17c198723085e
LukasChaloupecky <lukyn319@gmail.com>
2024-06-11 19:45:50 +0200

    armor reducer problem fixed

--- fighter/src/components/player/GameState.tsx
+++ fighter/src/components/player/GameState.tsx
@@ -1,3 +1,4 @@
+import { createContext } from "react"
 
 
 
@@ -7,7 +8,7 @@ type ArmorState = {
     pants: Armor,
     boots: Armor
 }
-type GameStateType = {
+export type GameStateType = {
     selectedWeapon: Weapon,
     selectedArmor: ArmorState, 
     weaponInventory: Weapon[],
@@ -16,7 +17,7 @@ type GameStateType = {
     currentSpot: number, // ? The spot in the room
     currentLevel: number // ? The level of the room
 }
-const InitialGameState: GameStateType = {
+export const InitialGameState: GameStateType = {
     selectedWeapon: WeaponsData[0],
     selectedArmor: {
         helmet: ArmorData[0],
@@ -33,34 +34,44 @@ const InitialGameState: GameStateType = {
 
 
 
-enum ActionEnum {
+export enum ActionEnum {
     CHANGE_WEAPON,
     CHANGE_ARMOR,
     CHANGE_SCORE,
     CHANGE_SPOT,
     CHANGE_LEVEL
 }
-type Action = {type: ActionEnum.CHANGE_WEAPON, WEAPON_INDEX: number}  // ? WEAPON_ID is in weapon inventory and id is for the item
-| {type : ActionEnum.CHANGE_ARMOR, ARMOR_INDEX: number} // ? ARMOR_ID is in armor inventory and id is for the item
-| {type : ActionEnum.CHANGE_SCORE, SCORE: number} // ? SCORE is the new score
-| {type : ActionEnum.CHANGE_SPOT, SPOT: number} // ? SPOT is the new spot
-| {type : ActionEnum.CHANGE_LEVEL, LEVEL: number} // ? LEVEL is the new level
+export type Action = 
+| {type : ActionEnum.CHANGE_WEAPON, WEAPON_INDEX: number}  // ? WEAPON_ID is in weapon inventory and id is for the item
+| {type : ActionEnum.CHANGE_ARMOR , ARMOR_INDEX: number} // ? ARMOR_ID is in armor inventory and id is for the item
+| {type : ActionEnum.CHANGE_SCORE , SCORE: number} // ? SCORE is the new score
+| {type : ActionEnum.CHANGE_SPOT  , SPOT: number} // ? SPOT is the new spot
+| {type : ActionEnum.CHANGE_LEVEL , LEVEL: number} // ? LEVEL is the new level
 
 
 
-const GameState = (state: GameStateType, action: Action) => {
+export const GameState = (state : GameStateType, action: Action) => {
     switch(action.type){
         case ActionEnum.CHANGE_WEAPON:
-            return {...state, selectedWeapon: state.weaponInventory[action.WEAPON_INDEX]}
+            return {...state, selectedWeapon: state.weaponInventory[action.WEAPON_INDEX]};
         case ActionEnum.CHANGE_ARMOR:
-            return {...state, selectedArmor: state.armorInventory[action.ARMOR_INDEX]}
+            const armor = state.armorInventory[action.ARMOR_INDEX];
+            return {...state, selectedArmor: {
+                helmet:      armor.type === ArmorType.HELMET      ? armor : state.selectedArmor.helmet,
+                breastplate: armor.type === ArmorType.BREASTPLATE ? armor : state.selectedArmor.breastplate,
+                pants:       armor.type === ArmorType.PANTS       ? armor : state.selectedArmor.pants,
+                boots:       armor.type === ArmorType.BOOTS       ? armor : state.selectedArmor.boots
+            }}; 
         case ActionEnum.CHANGE_SCORE:
-            return {...state, score: action.SCORE}
+            return {...state, score: action.SCORE};
         case ActionEnum.CHANGE_SPOT:
-            return {...state, currentSpot: action.SPOT}
+            return {...state, currentSpot: action.SPOT};
         case ActionEnum.CHANGE_LEVEL:
-            return {...state, currentLevel: action.LEVEL}
+            return {...state, currentLevel: action.LEVEL}; 
         default:
             return state
     }
-}
+};
+
+
+export const GameContext = createContext<{ state: GameStateType; dispatch: React.Dispatch<Action> } | undefined>(undefined);
