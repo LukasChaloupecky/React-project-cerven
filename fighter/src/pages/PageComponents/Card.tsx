19725adb68220a69acf1e6347b4bbbb69a5c3691
LukasChaloupecky <lukyn319@gmail.com>
2024-06-12 13:10:32 +0200

    images don't work

--- fighter/src/pages/PageComponents/Card.tsx
+++ fighter/src/pages/PageComponents/Card.tsx
@@ -1,6 +1,9 @@
+import { render } from "react-dom";
+
 const Card = ({title, imgUrl} : {title : string, imgUrl : string}) => {
     return (
         <div>
+            
             <h1>{title}</h1>
         </div>
     )
