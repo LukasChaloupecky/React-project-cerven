a468b994a406cb058a0c6be790197cf0a0a6999c
LukasChaloupecky <lukyn319@gmail.com>
2024-06-12 10:39:19 +0200

    ssss

--- fighter/src/components/methods/Fighting.tsx
+++ fighter/src/components/methods/Fighting.tsx
@@ -1,31 +1,29 @@
 import { Enemy } from "../enemy/Enemy";
 import { GameStateType, Rules } from "../player/GameState";
 
-export const FightHandler = ({gamestate, choice} : {gamestate : GameStateType, choice : FightChoice}) => {
-    const correct = Math.floor(Math.random() * 1); 
+export const FightHandler = ({gamestate, choice, enemy} : {gamestate : GameStateType, choice : FightChoice, enemy : Enemy}) => {
+    const correct = Math.floor(Math.random() * 2); 
     let damage : number = 0;
     if (correct === choice.valueOf()) {
-        damage = (choice === FightChoice.FastAttack) ? (gamestate.selectedWeapon.damage * Rules.matchingElementMultiplier)  : gamestate.selectedWeapon.damage;
+        damage = (enemy.element === gamestate.selectedWeapon.element) ? (gamestate.selectedWeapon.damage * Rules.matchingElementMultiplier)  : gamestate.selectedWeapon.damage;
     }
     return damage;
 }
 
 export const DefenseHandler = ({enemy, gamestate} : { enemy : Enemy, gamestate : GameStateType}) => {
-    const IsAttacked = Math.floor(Math.random() * 1); // TODO : Decide about the 1 => 50% chance of getting attacked
+    const IsAttacked = Math.floor(Math.random() *2 ); // TODO : Decide about the 1 => 50% chance of getting attacked
+    console.log(Math.random() + " " + IsAttacked);
     let HP : number = Rules.maxHP;
-
+    let attack : number = enemy.damage;
     if (IsAttacked === 1) {
-        let attack : number = enemy.damage;
-        let elementDefense : number = Math.floor(enemy.damage*Rules.elementProtectionMultiplier);
+        let elementDefense : number = enemy.damage*Rules.elementProtectionMultiplier;
 
         if (gamestate.selectedArmor.helmet.element === enemy.element)      attack -= elementDefense;
         if (gamestate.selectedArmor.breastplate.element === enemy.element) attack -= elementDefense;
         if (gamestate.selectedArmor.pants.element === enemy.element)       attack -= elementDefense;
         if (gamestate.selectedArmor.boots.element === enemy.element)       attack -= elementDefense;
-
-        HP -= attack;
     }
-    return HP;
+    return attack;
 }
 
 export enum FightChoice {
--- fighter/src/pages/Board.tsx
+++ fighter/src/pages/Board.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ActionEnum, GameContext, Rules } from "../components/player/GameState";
 import { DefenseHandler, FightChoice, FightHandler } from "../components/methods/Fighting";
 import { ArmorData } from "../data/ArmorData";
@@ -15,15 +15,17 @@ const Board = () => {
     const [PlayerTurn, setPlayerTurn] = useState(true);
 
     const HandleAttack = (choice : FightChoice) => {
-        const damage = FightHandler({gamestate: Reducer?.state, choice: choice});
+        const damage = FightHandler({gamestate: Reducer?.state, choice: choice, enemy : Enemy});
         setEnemy({...Enemy, hp: Enemy.hp - damage});
 
+        console.log("Enemy HP: " + Enemy.hp);
         console.log("Enemy HP: " + Enemy.hp);
 
         if (Enemy.hp <= 0) HandeWin();
         else setPlayerTurn(false);
     }
     const HandeWin = () => {
+        console.log("Win");
         Reducer?.dispatch({type: ActionEnum.CHANGE_SCORE, SCORE_DIFFERENCE: Enemy.score});
         Reducer?.dispatch({type: ActionEnum.IS_FIGHT, IS_FIGHT: false});
 
@@ -41,17 +43,25 @@ const Board = () => {
 
             Reducer.dispatch({type: ActionEnum.ADD_WEAPON, WEAPON: weapons[index]});
         }
+        Reducer.dispatch({type : ActionEnum.IS_FIGHT, IS_FIGHT: false})
+        setPlayerHP(Rules.maxHP);
     }
 
     const HandleDefense = () => {
-        setPlayerHP(DefenseHandler({enemy: Enemy, gamestate: Reducer?.state}));
+        setPlayerHP(PlayerHP - DefenseHandler({enemy: Enemy, gamestate: Reducer?.state}));
         console.log("Player HP: " + PlayerHP);
+        console.log("Enemy HP: " + Enemy.hp);
 
         setPlayerTurn(true);
     }
-    const HandeMove = () => {
+    const HandleMove = () => {
         const move = Math.floor(Math.random() * Rules.maxMove);
         Reducer?.dispatch({type: ActionEnum.CHANGE_SPOT, SPOT_DIFFERENCE: move});
+
+        setEnemy(Reducer?.state.Board[Reducer?.state.currentSpot]);
+
+        Reducer?.dispatch({type: ActionEnum.IS_FIGHT, IS_FIGHT: true});
+        setPlayerTurn(true);
     }
 
     return (
@@ -69,6 +79,7 @@ const Board = () => {
                     <div>
                         <button onClick={() => HandleAttack(FightChoice.FastAttack)}>FastAttack</button>     
                         <button onClick={() => HandleAttack(FightChoice.StrongAttack)}>StrongAttack</button>     
+                        
                     </div>           
                     :
                     <button onClick={() => HandleDefense()}>Defense</button>
@@ -78,9 +89,13 @@ const Board = () => {
             :
                 <div>
                 <h1>move</h1>
-                    <button onClick={() => HandeMove()}>Move</button>
+                    <button onClick={() => HandleMove()}>Move</button>
                 </div>
             }
+            <div>
+                <h1>Player : {PlayerHP}</h1>
+                <h1>Enemy : {Enemy.hp}</h1>
+            </div>
         </>
     );
 }
